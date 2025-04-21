735588bca75dfab4bc21ea801be351ec804c7b2d
André Santos <andremissional@gmail.com>
2025-04-21 15:56:37 -0300

    aula10_Treinamento05

--- aula08/scripts.js
+++ aula08/scripts.js
@@ -1,9 +1,18 @@
 // Tipos de função
 
-/*1º tipo: realiza uma tarefa, e não devolve nada
-*/
+/*1º tipo: realiza uma tarefa, e não devolve nada*/
+
 function nome(){
     console.log('André');
 };
 
 nome();
+
+//Faz um calculo ou operação e retona o valor
+function MultipilcarPor2(valor){
+    return valor*2;
+};
+
+let resultado = MultipilcarPor2(40);
+
+console.log(resultado);
