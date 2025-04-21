983022f13f596c8ea125a189c9d5115c00a538f3
André Santos <andremissional@gmail.com>
2025-04-21 13:51:28 -0300

    Update scripts.js

---
+++ aula02/scripts.js
@@ -0,0 +1,10 @@
+//Arrays é conjunto de dados que pode ser acessado pelos indices
+// indeces são posições exatas aonde estão os dados
+
+let familia = [true,25,65,85,'André',250];
+console.log(familia.length);
+console.log(familia[4]);
+
+let melhorAmigo = ['Felipe',36,'Niterói'];
+console.log(melhorAmigo.length);
+console.log(melhorAmigo[0]);
