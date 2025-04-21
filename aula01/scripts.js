33799a6117082f0e66ad785948e9268dd998a3fc
André Santos <andremissional@gmail.com>
2025-04-21 13:37:30 -0300

    aula02

--- aula01/scripts.js
+++ aula01/scripts.js
@@ -3,11 +3,42 @@ let idade = 37;
 let estaAprovado = true;
 let sobrenome = 'Santos';
 
-let aluno = {
-        nome: 'André',
-        idade: 37,
-        estaAprovado: true,
-        sobrenome:'Santos'
+let aluno ={
+        nome: nome,
+        idade: idade,
+        estaAprovado: estaAprovado,
+        sobrenome:sobrenome,
 };
 
 console.log(aluno);
+
+const marca = 'mustang';
+const modelo = 'shelb67';
+const ano = 1967;
+const cor = 'cinza';
+let valor = 150.000;
+
+let carro ={
+        marca: marca,
+        modelo: modelo,
+        ano: ano,
+        cor: cor,
+        valor: valor,
+};
+
+ console.log(carro);
+
+
+ const pai = 'Luiz';
+ const mae = 'Josélia';
+ const esposa = 'Mírian';
+ const irmao = 'Gabriel';
+
+ let familia ={
+        pai: pai,
+        mae: mae,
+        esposa: esposa,
+        irmao: irmao,
+ };
+
+ console.log(familia);
